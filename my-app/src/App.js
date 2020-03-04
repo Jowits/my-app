7dc67b3a27ea9cbaff3cde0544ed734a2d25d823
Jowita <jowita.lapies@gmail.com>
2020-03-04 14:48:02 +0000

    next stage

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,15 +1,15 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { Component } from "react";
+import Nav from "./Nav";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>My App</h1>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Nav />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ my-app/src/Nav.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Nav = () => {
+  return (
+    <div className="navWrapper">
+      <span className="headerText">My App</span>
+      <div className="TwirlyPig">
+        <a href="">
+          <img className="App-logo" alt="x" />
+        </a>
+      </div>
+      <span className="normalText">A React App </span>
+    </div>
+  );
+};
+
+export default Nav;
